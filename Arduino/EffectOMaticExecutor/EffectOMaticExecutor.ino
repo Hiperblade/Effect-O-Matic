eef86e5f909fc549f7fcd3664c5b19eb7893ede4
Giorgio Amadei <hiperblade.2501@gmail.com>
2014-07-04 10:12:21 +0200

    Gestione pin digitali

--- Arduino/EffectOMaticExecutor/EffectOMaticExecutor.ino
+++ Arduino/EffectOMaticExecutor/EffectOMaticExecutor.ino
@@ -3,8 +3,14 @@
 //const int pwnPins[] = {3, 5, 6, 9, 10, 11}; // Arduino UNO
 //const int pwnPins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}; // Arduino DUE / MEGA
 const int pwnPins[] = {3, 5, 6, 9, 10};
+const int digitalPins[] = {};
 
-const int numberPins = sizeof(pwnPins) / sizeof(int);
+// soglia oltre la quale viene settato a HIGH il pin digitale
+const int digitalThreshold = 200;
+
+const int numberPwnPins = sizeof(pwnPins) / sizeof(int);
+const int numberDigitalPins = sizeof(pwnPins) / sizeof(int);
+const int numberPins = numberPwnPins + numberDigitalPins;
 char data[numberPins];
 
 void setup()
@@ -12,20 +18,40 @@ void setup()
   // initialize the serial communication:
   Serial.begin(9600);
   
-  for(int i = 0; i < numberPins; i++)
+  for(int i = 0; i < numberPwnPins; i++)
   {
     pinMode(pwnPins[i], OUTPUT);
   }
+  for(int i = 0; i < numberDigitalPins; i++)
+  {  
+    pinMode(digitalPins[i], OUTPUT);
+  }
 }
 
-void loop() {
+void loop()
+{
   // check if data has been sent from the computer:
-  if (Serial.available()) {
+  if (Serial.available())
+  {
     Serial.readBytes(data, numberPins);
 
     for(int i = 0; i < numberPins; i++)
     {
-      analogWrite(pwnPins[i], (byte)data[i]);
+      if(i < numberPwnPins)
+      {
+        analogWrite(pwnPins[i], (byte)data[i]);
+      }
+      else
+      {
+        if((byte)data[i] > digitalThreshold)
+        {
+          digitalWrite(digitalPins[i - numberPwnPins], HIGH);
+        }
+        else
+        {
+          digitalWrite(digitalPins[i - numberPwnPins], LOW);
+        }
+      }
     }
   }
 }
